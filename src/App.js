b5fe53380a8e56a874d1d738b49ff879f2411f99
barackmaund1 <pmaundu49@gmail.co>
2020-11-16 15:16:35 +0300

    build banner with react owl carousel

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 import Header from './Header';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Banner from './Banner';
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         {/*header*/}
+       
         <Header />
+
         {/*banner*/}
+        <Banner />
         {/*About*/}
         {/*What we do*/}
         {/*Our mission*/}
---
+++ src/Banner.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import OwlCarousel from "react-owl-carousel";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css"; 
+import './Banner.css'
+
+
+const Banner = () => {
+    return (
+      <div>
+        <div class="container-fluid">
+          <div className="row title" style={{ marginBottom: "20px" }}>
+            <div class="col-sm-12 btn btn-info">
+              Owl Carousel In React Application
+            </div>
+          </div>
+        </div>
+        <div class="container-fluid">
+          <OwlCarousel
+            items={3}
+            className="owl-theme"
+            items={3}
+            margin={8}
+            autoplay={true}
+          >
+            <div>
+              <img className="img" src={"assets/img/1.jpeg"} />
+            </div>
+
+            <div>
+              <img className="img" src={"assets/img/2.jpeg"} />
+            </div>
+
+            <div>
+              <img className="img" src={"assets/img/4.jpeg"} />
+            </div>
+
+            <div>
+              <img className="img" src={"assets/img/3.jpeg"} />
+            </div>
+
+            <div>
+              <img className="img" src={"assets/img/5.jpeg"} />
+            </div>
+
+            <div>
+              <img className="img" src={"assets/img/6.jpeg"} />
+            </div>
+
+            <div>
+              <img className="img" src={"assets/img/download.jpeg"} />
+            </div>
+          </OwlCarousel>
+        </div>
+      </div>
+    );
+}
+
+export default Banner
